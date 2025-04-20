9cf182cc54ecccad1da2496b8d730950fdc51316
alex <alexthomas1349@gmail.com>
2025-04-19 23:16:33 -0400

    tot

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -300,11 +300,11 @@ function ProductList({ onHomeClick }) {
                                 fontWeight: 'bold',
                                 marginLeft: '-10px'
                             }}>
-                                {totalQuantity}
+                                <p>6</p>{totalQuantity}
                             </span>
                         )}</h1></a>
                         </div>
-                        <div><p>6</p></div>
+                        
                         </div>
                         </div>
             {!showCart ? (
