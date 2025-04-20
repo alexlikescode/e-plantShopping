4bbb4130e72cecc44008ad058a0d13a9e9f8f343
alex <alexthomas1349@gmail.com>
2025-04-19 21:32:24 -0400

    adasd

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -299,7 +299,7 @@ function ProductList({ onHomeClick }) {
                                             <p>{plant.description}</p>
                                             <p>{plant.cost}</p>
                                         {/*Similarly like the above plant.name show other details like description and cost*/}
-                                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                        <button style={{backgroundColor:alreadyInCart(plant.name)?"gray":"#615EFC"}} disabled={alreadyInCart(plant.name)? true:false} className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                                     </div>
                                 ))}
                             </div>
