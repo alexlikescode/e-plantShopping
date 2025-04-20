e9ec17d0385d52243ff1d059444eaac0ae938745
alex <alexthomas1349@gmail.com>
2025-04-19 21:18:04 -0400

    sgsf

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { useSelector, useDispatch } from 'react-redux';
+import { addItem } from './CartSlice';
 
 function ProductList({ onHomeClick }) {
     const dispatch = useDispatch();
