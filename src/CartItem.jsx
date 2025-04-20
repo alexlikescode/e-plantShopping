27de56ef1717848c275de06706e64b30ee2e0675
alex <alexthomas1349@gmail.com>
2025-04-19 21:00:18 -0400

    sdfsd

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -15,6 +15,9 @@ const CartItem = ({ onContinueShopping }) => {
     });
     return total;
   };
+  const handleContinueShopping = (e) => {
+    onContinueShopping(e);
+};
 
   const handleCheckoutShopping = (e) => {
     alert('Functionality to be added for future reference');
@@ -67,7 +70,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
