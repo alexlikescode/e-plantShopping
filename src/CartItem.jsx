dbc8da10d1a7392cd0638ffac01d3eb765d966f4
alex <alexthomas1349@gmail.com>
2025-04-20 00:28:08 -0400

    tot

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -63,7 +63,7 @@ const CartItem = ({ onContinueShopping }) => {
                 <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
               </div>
               <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
-              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+              <button className="cart-item-delete" onClick={() => handleRemove(item.name)}>Delete</button>
             </div>
           </div>
         ))}
